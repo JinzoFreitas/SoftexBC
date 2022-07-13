24177fe156ccd69e311f2940b423ddab6e241edb
Janderson S. Freitas <95733951+JinzoFreitas@users.noreply.github.com>
2022-07-13 20:59:40 -0300

    Update TAD_complex.py

--- 1 - LP - POO/2 - Alg e estr de dados/Tarefa 5/TAD_complex.py
+++ 1 - LP - POO/2 - Alg e estr de dados/Tarefa 5/TAD_complex.py
@@ -1,16 +1,9 @@
 #
 #
 
-
-from cmath import sqrt
-from turtle import ycor
-
-
-class Complx:
-    def __init__(self, x, y) -> complex:
-       self = x + y*sqrt(-1) 
-       self.x = x
-       self.y = y
-        
-n1 = Complx(1,3)
+class myComplex:
+    def __init__(self, x, y):
+        self = complex(x,y)
+      
+n1 = myComplex(1,3)
 print(n1)
