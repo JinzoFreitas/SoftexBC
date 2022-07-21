a8635d96d403bfa1caeaa439de87c6b0b4827ec5
Janderson S. Freitas <95733951+JinzoFreitas@users.noreply.github.com>
2022-07-20 22:55:52 -0300

    Update ordenacao.py

--- 1 - LP - POO/exercicios/ordenacao.py
+++ 1 - LP - POO/exercicios/ordenacao.py
@@ -4,19 +4,21 @@
 
 def main():
   vetor = []
-  menor_valor = 0
+  aux = 0
   for i in range(10):
     vetor.append(int(input("Digite um valor para o vetor: ")))
   
+  print("Vetor inicial:")
   print(vetor)  
   
   for i1 in range(9):
-    menor_valor = vetor[i1]
     for i2 in range(i1+1,10):
-      if (vetor[i2] < menor_valor):
-        menor_valor = vetor[i2]
+      if (vetor[i2] < vetor[i1]):
+        aux = vetor[i2]
         vetor[i2] = vetor[i1]     
-        vetor[i1] = menor_valor    
+        vetor[i1] = aux    
+  
+  print("Vetor ordenado:")
   print(vetor)    
 
 main()
