9f57e897c90046bb472e6acd14bdb8f9bfa0b2d5
Janderson S. Freitas <95733951+JinzoFreitas@users.noreply.github.com>
2022-07-21 01:21:43 -0300

    update calc

--- 1 - LP - POO/2- LP/Atividade 4/calc.py
+++ 1 - LP - POO/2- LP/Atividade 4/calc.py
@@ -2,8 +2,6 @@
 # Janderson Freitas
 # Atividade 4 - Calc
 
-
-
 '''
 Faça uma função calculadora de dois números com três parâmetros: os dois primeiros serão os números da operação e o terceiro será a entrada que definirá a operação a ser executada. Considera a seguinte definição:
 1. Soma
