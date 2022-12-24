2bdad1ef84eec8ac8e1184022332adac1c0fafe6
Janderson Freitas <sfreitas.janderson@gmail.com>
2022-12-23 21:43:36 -0300

    create dir

---
+++ 2 - JavaScript/Desenvolvimento 6/desenvolvimento5.js
@@ -0,0 +1,16 @@
+/*Softex - Recife
+Janderson Freitas
+JavaScript
+Desenvolvimento 5 - Calc */
+
+//Importação prompt
+/*
+const input = require('prompt-sync')();
+
+let num1 = input("Enter the first value: ");
+let op = input("Enter the type of operation : ");
+let num2 = input("Enter the second value: ");
+*/
+
+var entrada=console.log(prompt('Input'));
+console.log(entrada)
